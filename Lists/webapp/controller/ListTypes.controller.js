8e9280190093703a2457a1b507ff2f39547a60b4
Sergio Ortiz <sergiohoo@gmail.com>
2021-04-28 16:12:32 +0000

    Lab17.13 terminado

--- Lists/webapp/controller/ListTypes.controller.js
+++ Lists/webapp/controller/ListTypes.controller.js
@@ -33,12 +33,43 @@ sap.ui.define([
                 }
                 else {
                     var textMessage = i18nModel.getText("selection");
-                    for(var item in selectedItems) {
+                    for (var item in selectedItems) {
                         var context = selectedItems[item].getBindingContext();
                         var oContext = context.getObject();
                         textMessage = textMessage + " - " + oContext.Material;
                     }
 
+                    sap.m.MessageToast.show(textMessage, {duration : 20000});
+                }
+            },
+
+            onDeleteSelectedRow: function () {
+                var standardList = this.getView().byId("standardList");
+                var selectedItems = standardList.getSelectedItems();
+
+                var i18nModel = this.getView().getModel("i18n").getResourceBundle();
+
+                if(selectedItems.length === 0) {
+                    sap.m.MessageToast.show(i18nModel.getText("noSelection"), {duration : 20000});
+                }
+                else {
+                    var textMessage = i18nModel.getText("selection");
+                    var model = this.getView().getModel();
+                    var products = model.getProperty("/Products");
+
+                    var arrayId = [];
+
+                    for (var item in selectedItems) {
+                        var context = selectedItems[item].getBindingContext();
+                        var oContext = context.getObject();
+                        arrayId.push(oContext.Id);
+                        textMessage = textMessage + " - " + oContext.Material;
+                    };
+                    products = products.filter (
+                        function (p) {
+                        return !arrayId.includes(p.Id);
+                    });
+                    model.setProperty("/Products", products);
                     sap.m.MessageToast.show(textMessage, {duration : 20000});
                 }
             }
