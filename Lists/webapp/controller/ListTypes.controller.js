83efa84dec68b2ee043841d040b913fc7b9aaa13
Sergio Ortiz <sergiohoo@gmail.com>
2021-04-28 14:28:42 +0000

    Lab17.12 terminado

--- Lists/webapp/controller/ListTypes.controller.js
+++ Lists/webapp/controller/ListTypes.controller.js
@@ -20,6 +20,27 @@ sap.ui.define([
                 });
                 
                 return groupHeaderListItem;
+            },
+
+            onShowSelectedRow: function () {
+                var standardList = this.getView().byId("standardList");
+                var selectedItems = standardList.getSelectedItems();
+
+                var i18nModel = this.getView().getModel("i18n").getResourceBundle();
+
+                if(selectedItems.length === 0) {
+                    sap.m.MessageToast.show(i18nModel.getText("noSelection"), {duration : 20000});
+                }
+                else {
+                    var textMessage = i18nModel.getText("selection");
+                    for(var item in selectedItems) {
+                        var context = selectedItems[item].getBindingContext();
+                        var oContext = context.getObject();
+                        textMessage = textMessage + " - " + oContext.Material;
+                    }
+
+                    sap.m.MessageToast.show(textMessage, {duration : 20000});
+                }
             }
 		});
 	});
